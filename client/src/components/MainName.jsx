dee9285d0ae54cdad0842b1d5fe69062c5da8a9c
Ankit Kumar <ankit1842kumar@gmail.com>
2025-07-18 21:05:23 +0530

    some url fiexs

--- client/src/components/MainName.jsx
+++ client/src/components/MainName.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { motion } from 'framer-motion'
 
 function MainName() {
+  
   return (
     // <div className='fixed top-0 left-0 right-0 z-50 bg-white/30 backdrop-blur-md shadow-2xs'>
     //   <img
--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -4,6 +4,8 @@ import { useNavigate } from 'react-router-dom'
 
 function NavBar() {
     const navigate = useNavigate()
+   const frontendURL = import.meta.env.VITE_FRONTEND_PORT
+
   return (
     <motion.div
      initial={{ opacity: 0, y: -50 }}
@@ -12,7 +14,7 @@ function NavBar() {
     >
       <header 
       className="fixed top-1 left-1/2 -translate-x-1/2 w-[90%] max-w-5xl bg-white/30 backdrop-blur-md shadow-lg z-50 px-6  flex items-center justify-between px-6 py-3 md:py-4 shadow max-w-5xl rounded-full mx-auto w-full bg-white">
-    <a href="#">
+    <a href={frontendURL}>
         {/* <img src="https://raw.githubusercontent.com/prebuiltui/prebuiltui/main/assets/dummyLogo/prebuiltuiDummyLogo.svg" /> */}
         <div className="flex items-center justify-center gap-2 ">
     <img
@@ -24,7 +26,7 @@ function NavBar() {
   </div>
     </a>
     <nav id="menu" class="max-md:absolute max-md:top-0 max-md:left-0 max-md:overflow-hidden items-center justify-center max-md:h-full max-md:w-0 transition-[width] flex-col md:flex-row flex gap-8 text-gray-900 text-sm font-normal">
-        <a class="hover:text-indigo-600" href="#home">
+        <a class="hover:text-indigo-600" href={frontendURL}>
             Home
         </a>
         <a class="hover:text-indigo-600" href="#reviews">
@@ -33,7 +35,7 @@ function NavBar() {
         <a class="hover:text-indigo-600" href="#faqs">
             FAQs
         </a>
-        <a class="hover:text-indigo-600" href="about">
+        <a class="hover:text-indigo-600" href={`${frontendURL}/about`}>
             About
         </a>
         <a class="hover:text-indigo-600" href="disclaimer">
